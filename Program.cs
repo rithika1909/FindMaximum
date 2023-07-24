1b62d9c83eda46e9ff38f74cb5d63513564a9a9f
Rithika L <rithika.l@tvsnext.com>
2023-07-24 14:44:04 +0530

    [Rithika] Add : Implemented

--- Program.cs
+++ Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+namespace FindMax
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Operation operation = new Operation();
+            operation.FindMaxInteger(1, 2, 3);
+            operation.FindMaxFloat(1.2f, 1.8f, 1.1f);
+            operation.FindMaxString("a", "b", "c");
+
+            GenericOperation genericOperation = new GenericOperation();
+            Console.WriteLine("\n\nGeneric:\n");
+            genericOperation.FindMax<int>(1, 2, 3);
+            genericOperation.FindMax<float>(1.1f,1.9f,1.4f);
+            genericOperation.FindMax<string>("a","b","c");
+        }
+    }
+}
