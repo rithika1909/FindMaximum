1e8b52519e86d9006223b0487189f152d25b182d
rithika1909 <113461039+rithika1909@users.noreply.github.com>
2023-07-24 14:36:33 +0530

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using System;
+namespace FindMax
+{
+    class Program
+    {
+        static void Main(string[] args) 
+        {
+            Operation operation = new Operation();
+            operation.FindMaxInteger(1, 2, 3);
+            operation.FindMaxFloat(1.2f, 1.8f, 1.1f);
+            operation.FindMaxString("a", "b", "c");
+        }
+    }
+}
